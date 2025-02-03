89be63b291b851b9cfdd84a9ae2ebca9e8499764
DmitriyPolyanskiy <47323019+DmitriyPolyanskiy@users.noreply.github.com>
2025-02-03 11:36:32 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,7 +4,47 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    const int n = 3;
+
+    //1. Одномерный массив плавающих чисел;
+    float array1[n];
+
+    //2. Указатель на тип float;
+    float* pointer2;
+
+    //3. Статический одномерный массив беззнаковых целых чисел;
+    static uint32_t array3[n];
+
+    //4. Указатель на массив unsignedint;
+    uint32_t (*pointer4)[n];
+
+    //5. Трехмерный массив символов;
+    char array5[n][n][n];
+
+    //6. Указатель на двумерный массив символов.
+    char (*pointer6)[n][n];
+
+//    cout << "Hello World!" << endl;
+
+    // Проверить, заполнить, проверить
+    for(int i = 0; i < n; i++){
+        cout << array1[i] << "\n";
+    }
+    for(int i = 0; i < n; i++){
+        cin >> array1[i];
+    }
+    for(int i = 0; i < n; i++){
+        cout << array1[i] << "\n";
+    }
+
+    // Указатель
+    pointer2 = (float*)&array1;
+    cout << "Указатели массива\n";
+    for(int i = 0; i < n; i++){
+        cout << &array1[i] << "\n";
+    }
+    cout << "Наш указатель:\n" << pointer2 << "\n";
+
     return 0;
 }
 
